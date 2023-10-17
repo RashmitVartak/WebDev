581956b60484e2b350f6305e56440ae8bd7082bc
RashmitVartak <rashmitvartak@gmail.com>
2023-10-18 01:07:31 +0530

    react installed, configured and created SideBar, Navbar

---
+++ exp7/my-react-app/src/MainContent.js
@@ -0,0 +1,13 @@
+// src/MainContent.js
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main>
+      <h2>Main Content</h2>
+      <p>This is the main content of my app.</p>
+    </main>
+  );
+}
+
+export default MainContent;
---
+++ exp7/my-react-app/src/Sidebar.js
@@ -0,0 +1,18 @@
+// src/Sidebar.js
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <aside>
+      <nav>
+        <ul>
+          <li>Menu Item 1</li>
+          <li>Menu Item 2</li>
+          <li>Menu Item 3</li>
+        </ul>
+      </nav>
+    </aside>
+  );
+}
+
+export default Sidebar;
