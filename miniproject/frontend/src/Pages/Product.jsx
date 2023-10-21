0a0f784bd545c70a154ab06f543ab7629d9092a1
RashmitVartak <rashmitvartak@gmail.com>
2023-10-21 11:16:48 +0530

    additional change

--- miniproject/frontend/src/Pages/Product.jsx
+++ miniproject/frontend/src/Pages/Product.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import all_product from '../Components/Assets/all_product'
+import ShopCategory from './ShopCategory'
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../Context/ShopContext'
+import Breadcrum from '../Components/Breadcrums/Breadcrum'
+import ProductDisplay from  '../Components/ProductDisplay/ProductDisplay'
 
 const Product = () => {
-    return(
+    const {all_product}= useContext(ShopContext);
+    const {productId} = useParams();
+    const product = all_product.find((e) => e.id === Number(productId)); 
+    useEffect( () => {console.log(productId);console.log(product)})
+        return(
         <div>
-
+            <Breadcrum product={product}/>
+            <ProductDisplay product={product}/>
         </div>
     )
 }
